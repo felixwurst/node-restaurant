c0a3c25598b0860c1df336a02c10b44fc7776dee
felixwurst <felix.wurst@gmail.com>
2020-09-28 18:15:23 +0200

    latest state

--- app.js
+++ app.js
@@ -5,16 +5,24 @@ const app = express();
 
 // modules
 const fs = require('fs');
-const emailSender = require('./modules/emailSender');
 const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const emailSender = require('./modules/emailSender');
 const adminRouter = require('./routes/adminRouter');
 
 // app-use
-app.use(express.urlencoded({extended: false})); // urlencoded aka bodyParser
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); // parses incoming requests with json payloads to an object
 app.use(express.static('./public')); // public-folder
 app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
 }));
+app.use(session({
+    secret: 'burger',
+    cookie: {}
+}));
+app.use(cookieParser());
 
 // ejs-views
 app.set('view engine', 'ejs');
@@ -24,9 +32,6 @@ app.set('views', './views');
 const myMeals = JSON.parse(fs.readFileSync('./meals.json')); // read json and parse to object
 
 // ---------------------------------------- routes ----------------------------------------
-// admin
-app.use('/admin', adminRouter.useAdminRouter(myMeals));
-
 // home
 app.get('/', (req, res) => {
     res.render('./main', {meals: myMeals});
@@ -42,7 +47,6 @@ app.get('/contact', (req, res) => {
     res.render('./contact', {sent: 1});
 });
 app.post('/contact', (req, res) => {
-    // console.log(req.body);
     const name = req.body.name;
     const email = req.body.email;
     const subject = req.body.subject;
@@ -57,6 +61,93 @@ app.post('/contact', (req, res) => {
         });       
     }
 });
+
+// meals
+app.get('/meal/:title', (req, res) => {
+    // get meal using title
+    const mealTitle = req.params.title; // e.g. Burger_Boys
+    const foundMeal = myMeals.find(meal => meal.title.trim().replace(/ /g, '_') == mealTitle);
+    // console.log(foundMeal);
+    // console.log(req.params.title);
+    if (foundMeal) {
+        res.render('./meal', {
+            mealTitle: foundMeal.title,
+            mealPrice: foundMeal.price,
+            mealImg: foundMeal.imgUrl,
+            mealDescription: foundMeal.description,
+            mealDetails: foundMeal.details
+        });
+    } else {
+        res.send("404: Not found");
+    }
+    // // get meal using id -> '/meal/:id'
+    // const idx = req.params.id; // e.g. 2
+    // if (myMeals[idx]) {
+    //     res.render('./meal', {
+    //         mealTitle: myMeals[idx].title,
+    //         mealDescription: myMeals[idx].description,
+    //         mealImg: myMeals[idx].imgUrl,
+    //         mealPrice: myMeals[idx].price
+    //     });
+    // } else {
+    //     res.send("404 Not found");
+    // }
+});
+
+// ---------------------------------------- login to admin-panel ----------------------------------------
+// login
+app.get('/login', (req, res) => {
+    // console.log(req.cookies);
+    if (req.cookies.burgerUser) { // { username: 'felix', password: '1234' }
+        const users = JSON.parse(fs.readFileSync('./users.json'));
+        const foundUser = users.find(user => user.username == req.cookies.burgerUser.username && user.password == req.cookies.burgerUser.password)    
+        if (foundUser) {
+            req.session.user = foundUser;
+            res.redirect('/admin'); // cookie exists & user is found in users.json
+        } else {
+            res.render('./login'); // cookie exists, but user is not found
+        }    
+    } else {
+        res.render('./login'); // no cookie is saved
+    }
+});
+app.post('/login', (req, res) => {
+    // console.log(req.session);
+    const users = JSON.parse(fs.readFileSync('./users.json'));
+    // solution with find -> returns the value of the first element in an array that pass a test
+    const foundUser = users.find(user => user.username == req.body.username && user.password == req.body.password)    
+    if (foundUser) {
+        req.session.user = foundUser;
+        res.cookie("burgerUser", foundUser);
+        // res.cookie("burgerUser", foundUser, {maxAge: 60000, httpOnly: true}); // cookie will delete after 60s
+        res.json("exist"); // -> login.ejs -> /admin
+    } else {
+        res.json("not exist"); // -> login.ejs -> alert
+    }
+    // solution with for-loop
+    // let check = false;
+    // for (let i = 0; i < users.length; i++) {
+    //     if (req.body.username == users[i].username && req.body.password == users[i].password) {
+    //         check = true;
+    //         break;
+    //     }
+    // }
+    // if (check) {
+    //     res.json("existing");
+    // } else {
+    //     res.json("not existing");
+    // }
+});
+
+// logout
+app.get('/logout', (req, res) => {
+    req.session.destroy(); // destroys session & logs out
+    res.clearCookie('burgerUser'); // deletes cookie
+    res.redirect('/'); 
+});
+
+// admin
+app.use('/admin', adminRouter.useAdminRouter(myMeals));
 
 // ---------------------------------------- localhost ----------------------------------------
 app.listen(3000, () => {
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,8 +1,8 @@
 // ---------------------------------------- setup ----------------------------------------
 // express
 const express = require('express');
+// router
 const adminRouter = express.Router();
-
 // modules
 const fs = require('fs'); // fileserver
 
@@ -10,36 +10,60 @@ const fs = require('fs'); // fileserver
 
 function useAdminRouter(myMeals) {
 
+    // check if there is a valid session, all admin-routes have to pass this middleware 
+    adminRouter.use((req, res, next) => {
+        if (req.session.user) {
+            next();
+        } else {
+            res.redirect('/');
+        }
+    })
+
+    // admin-panel -> links to add, delete & edit meal
+    adminRouter.get('/', (req, res) => {
+        res.render('./admin');
+    });
+
     // add meal
     adminRouter.get('/addmeal', (req, res) => {
         res.render('./adminAddMeal', {meals: myMeals});
     });
     adminRouter.post('/addmeal', (req, res) => {
+        // inputs
         const mealTitle = req.body.mealTitle;
         const mealDescription = req.body.mealDescription;
         const mealPrice = req.body.mealPrice;
-        // console.log(req.body);
-        // console.log(req.files.mealImg);
+        const mealDetails = req.body.mealDetails;
+        // if all inputs are filled out
         if (mealTitle && mealDescription && mealPrice && req.files) {
-            const mealImg = req.files.mealImg;
-            console.log(mealImg);
-            // cuts the imagename, starts from the last dot -> .png / .jpeg
-            let ext = mealImg.name.substr(mealImg.name.lastIndexOf('.')); 
-            // replaces ' ' with '_', adds the number of myMeals.length and the extension
-            mealImg.mv('./public/uploadedFiles/' + mealTitle.replace(/ /g, '_') + myMeals.length + ext).then(() => { 
-                let obj = {
-                    title: mealTitle,
-                    description: mealDescription,
-                    imgUrl: '/uploadedFiles/' + mealTitle.replace(/ /g, '_') + myMeals.length + ext,
-                    price: mealPrice
-                }
-                myMeals.push(obj);
-                fs.writeFileSync('./meals.json', JSON.stringify(myMeals));
-                res.redirect('/admin/addmeal');
-            }).catch(error => {
-                console.log(error);
-                res.send(error.message);
-            })
+            const mealExist = myMeals.find(meal => meal.title == mealTitle); 
+            // if meal title still exists 
+            if (mealExist) {
+                console.log("meal title still exists");
+                res.send("meal title still exists");    
+            // if meal title is a new one
+            } else {
+                const mealImg = req.files.mealImg;
+                // cuts name of uploaded image, starting from last dot -> .png / .jpeg
+                let ext = mealImg.name.substr(mealImg.name.lastIndexOf('.')); 
+                // replaces ' ' with '_', adds the number of myMeals.length and the extension
+                mealImg.mv('./public/uploadedFiles/' + mealTitle.replace(/ /g, '_') + myMeals.length + ext).then(() => { 
+                    let obj = {
+                        title: mealTitle,
+                        price: mealPrice,
+                        imgUrl: '/uploadedFiles/' + mealTitle.replace(/ /g, '_') + myMeals.length + ext,
+                        description: mealDescription,
+                        details: mealDetails // tinyMCE-input
+                    }
+                    myMeals.push(obj);
+                    fs.writeFileSync('./meals.json', JSON.stringify(myMeals));
+                    res.redirect('/admin/addmeal');
+                }).catch(error => {
+                    console.log(error);
+                    res.send(error.message);
+                })
+            }
+        // if the inputs are not filled out
         } else {
             res.send("meal data is not complete");    
         }
@@ -51,12 +75,19 @@ function useAdminRouter(myMeals) {
     });
     adminRouter.post('/deletemeal', (req, res) => {
         const idx = req.body.mealid;
+        // deletes image from storage with unlink
         fs.unlink('./public' + myMeals[idx].imgUrl, function(err) {
             if (err) {
                 return res.status(500).send(err);
             }
             console.log('Image deleted!');
         });
+        // deletes image from storage with unlinkSync & try/catch
+        // try {
+        //     fs.unlinkSync('./public' + myMeals[idx].imgUrl);
+        // } catch (error) {
+        //     console.log('error');
+        // }
         myMeals.splice(idx, 1);
         fs.writeFileSync('./meals.json', JSON.stringify(myMeals));
         res.sendStatus(200);
@@ -67,51 +98,43 @@ function useAdminRouter(myMeals) {
         res.render('./adminEditMeal', {meals: myMeals});
     });
     adminRouter.post('/editmeal', (req, res) => {
-        let btnIdx = req.body.editBtn.replace("save", "")
-        // console.log(btnIdx);
-        // console.log(req.body.mealTitle[btnIdx]);
-        // console.log(req.body.mealDescription[btnIdx]);
-        // console.log(req.body.mealPrice[btnIdx]);
-        // console.log(req.files);
-
-        // image upload
+        // input-data
+        myMeals[req.body.mealid].title = req.body.mealTitle;
+        myMeals[req.body.mealid].description = req.body.mealDescription;
+        myMeals[req.body.mealid].price = req.body.mealPrice;
+        // updates meal with new image
         if (req.files) {
             const mealImg = req.files.mealImg;
-            const mealTitle = req.body.mealTitle[btnIdx];
-            // console.log(mealImg);
-            // console.log(mealTitle);
-
-            // delete old image
-            fs.unlink('./public' + myMeals[btnIdx].imgUrl, function(err) {
-                if (err) {
-                    return res.status(500).send(err);
-                }
-                console.log('Image deleted!');
-            });
-            // save and rename new image
+            const mealTitle = req.body.mealTitle;
+            // deletes old image from storage
+            fs.unlinkSync('./public' + myMeals[req.body.mealid].imgUrl);
+            // saves & renames new image
             let ext = mealImg.name.substr(mealImg.name.lastIndexOf('.')); 
-            mealImg.mv('./public/uploadedFiles/' + mealTitle.replace(/ /g, '_') + myMeals.length + ext).then(() => {
-                // save inputs to meals.json & redirect
-                myMeals[btnIdx].imgUrl = '/uploadedFiles/' + mealTitle.replace(/ /g, '_') + myMeals.length + ext;
-                myMeals[btnIdx].title = req.body.mealTitle[btnIdx];
-                myMeals[btnIdx].description = req.body.mealDescription[btnIdx];
-                myMeals[btnIdx].price = req.body.mealPrice[btnIdx];
+            mealImg.mv('./public/uploadedFiles/' + mealTitle.replace(/ /g, '_') + req.body.mealid + ext).then(() => {
+                myMeals[req.body.mealid].imgUrl = '/uploadedFiles/' + mealTitle.replace(/ /g, '_') + req.body.mealid + ext;
                 fs.writeFileSync('./meals.json', JSON.stringify(myMeals));
-                res.redirect('/admin/editmeal');
+                res.json(myMeals[req.body.mealid].imgUrl);
             }).catch(error => {
-                console.log(error);
-                res.send(error.message);
+                res.sendStatus(500);
             })
+        // updates meal without new image
         } else {
-            // save inputs to meals.json & redirect
-            myMeals[btnIdx].title = req.body.mealTitle[btnIdx];
-            myMeals[btnIdx].description = req.body.mealDescription[btnIdx];
-            myMeals[btnIdx].price = req.body.mealPrice[btnIdx];
             fs.writeFileSync('./meals.json', JSON.stringify(myMeals));
-            res.redirect('/admin/editmeal');
+            res.json(myMeals[req.body.mealid].imgUrl);
         }
     });
 
+    // checkmealname
+    adminRouter.post('/checkmealname', (req, res) => {
+        // console.log(req.body);
+        const mealExist = myMeals.find(meal => meal.title == req.body.mealTitle);
+        if (mealExist) {
+            res.json("meal title exist");
+        } else {
+            res.json("meal title do not exist");
+        }
+    });
+    
     return adminRouter;
 }
 
